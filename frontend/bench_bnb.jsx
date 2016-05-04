b2abb1da53a9a2416f96ad627a44e93221a93a67
Albert Nguyen <Albert.la.nguyen@gmail.com>
2016-05-03 21:18:36 -0700

    created frontend file, configured webpack

---
+++ frontend/bench_bnb.jsx
@@ -0,0 +1,7 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+
+document.addEventListener("DOMContentLoaded", function(){
+  ReactDOM.render(<div>Hello World</div>, document.getElementById('root'));
+});
